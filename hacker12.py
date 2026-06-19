73ecbcf16ff8ab5fddba06bb9ad8c29053217303
YEDLA SAI BHANU <yedlasaibhanu@gmail.com>
2026-06-19 15:15:39 +0530

    Create hacker12.py

---
+++ hacker12.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    from collections import Counter
+    
+    # 1. Calculate total pairs
+    total_pairs = sum(count // 2 for count in Counter(ar).values())
+    
+    return total_pairs
+    
+    # 2. Subtract paired elements from the total to get leftovers
+    
+
+
+
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
