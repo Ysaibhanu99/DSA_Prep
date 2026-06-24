6a4c4fdf0f40ce43eae67732df2ad903f171c30c
YEDLA SAI BHANU <yedlasaibhanu@gmail.com>
2026-06-24 10:10:54 +0530

    Add hurdleRace function to calculate drink doses
    Implement hurdleRace function to calculate drink doses based on hurdle heights.

---
+++ hacker20.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hurdleRace' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY height
+#
+
+def hurdleRace(k, height):
+    higher_heights = [h for h in height if h > k]
+    if len(higher_heights)==0:
+        drink_doses=0
+    else:
+        drink_doses= max(higher_heights)-k
+    return drink_doses
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    height = list(map(int, input().rstrip().split()))
+
+    result = hurdleRace(k, height)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
